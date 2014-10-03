d454f36af9be53a68120abf81dfdd2962ac032df
Noor Mohiuddin <noor.bilal@gmail.com>
2014-10-02 22:52:48 -0400

    Merged with incoming changes

--- united-responders/WebContent/WEB-INF/map.js
+++ united-responders/WebContent/WEB-INF/map.js
@@ -198,6 +198,7 @@ function createMarker(location, tooltip, mapsAPIURL){
 }
 
 function initialize() {
+	initializeGUIElements();
 	var karachiLatLng = getLatLng(24.866859, 67.013189);
 	
 	//TODO: Get this from the UI
@@ -230,6 +231,35 @@ function initialize() {
 	//TODO: use an enum
 	//TODO: use the location address in the tooltip
 	createMarker(emergency.location, "Emergency", "d_map_pin_letter&chld=E|FF8000");
+}
+
+function initializeGUIElements(){
+	
+	//First find all checkboxes.
+	for(var i = 1; i<=5 ; i++){
+		document.getElementById("emergencyType"+i).addEventListener('change',checkboxChange)
+	}
+	
+}
+
+function checkboxChange(){
+	var emergencyArray = [];
+	
+	for(var i = 1; i<=5 ; i++){
+		var theBox = document.getElementById("emergencyType"+i);
+		if(theBox.checked){
+			emergencyArray.push(theBox.value)
+		}
+	}
+	
+	updateAmbulances(emergencyArray)
+}
+
+function updateAmbulances(emergencyArray){
+	document.getElementById("emergencyNotes").value = "";
+	for(var i = 0 ; i < emergencyArray.length ; i++){
+		document.getElementById("emergencyNotes").value = document.getElementById("emergencyNotes").value + " " + emergencyArray[i];
+	}
 }
 
 //Initialize
